e5849b22576d4d66a4461b9d5cd6fb14177b3d13
S3L3CT3DLoves <153332410+S3L3CT3DLoves@users.noreply.github.com>
2025-01-20 10:21:29 +0100

    update MyIp to remove dependency

--- plugins/myIp/myIp.py
+++ plugins/myIp/myIp.py
@@ -1,12 +1,5 @@
 import sys
 import urllib.request
 
-try:
-    import stashapi.log as log
-except ModuleNotFoundError:
-    print("If you have pip (normally installed with python), run this command in a terminal (cmd): pip install stashapp-tools", file=sys.stderr)
-    sys.exit()
-
-
 external_ip = urllib.request.urlopen('https://v4.ident.me/').read().decode('utf8')
-log.info(f"Current IP : {external_ip}")
+print("\x01{}\x02".format("i"), f"Current IP : {external_ip}", file=sys.stderr, flush=True)
