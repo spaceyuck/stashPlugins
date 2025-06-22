236e2bc20a479201246b92856a66b0ccefcbbe03
spaceyuck <oss@spassshuttle.de>
2025-06-22 12:22:16 +0200

    [easyTag] clear config and load config from file should work again
    These were probably broken since the transition from LocalStorage to server-side storage.
    The reload triggered immediately, not waiting for saveConfig to complete, cancelling the request and not actually saving

--- plugins/QuickEdit/quickedit_config.js
+++ plugins/QuickEdit/quickedit_config.js
@@ -156,7 +156,7 @@ class ButtonsConfig{
         setTimeout(function() { URL.revokeObjectURL(a.href); }, 1500);
     }
 
-    loadConfigFromFile(fileContent){
+    async loadConfigFromFile(fileContent){
         let loadedConfig = JSON.parse(fileContent)
 
         if("groups" in loadedConfig && "tags" in loadedConfig){
@@ -164,14 +164,14 @@ class ButtonsConfig{
             Object.keys(this.groups).forEach((key) => this.groups[key] = GroupConfiguration.fromSavedData(this.groups[key], key))
             this.tags = loadedConfig.tags
             Object.keys(this.tags).forEach((key) => this.tags[key] = TagConfiguration.fromSavedData(this.tags[key]))
-            this.saveConfig()
+            await this.saveConfig()
         }
     }
 
-    clearConfig(){
+    async clearConfig(){
         this.groups = {}
         this.tags = []
-        this.saveConfig()
+        await this.saveConfig()
     }
 }
 
@@ -225,8 +225,9 @@ class ButtonsConfigUI{
         this.BTNCFG_CONTAINER.querySelector("#configModalClearConfig").addEventListener("click", (e) => {
             e.preventDefault()
             if(confirm("This will delete your QuickEdit config permanently. Are you sure ?")){
-                this.buttonsConfig.clearConfig()
-                window.location.reload()
+                this.buttonsConfig.clearConfig().then(() => {
+                    window.location.reload();
+                });
             }
         })
     }
